854e4d7737e2437e22a2b63e2565c09177900361
Andrea Bertagnolli <andrea.bertagnolli@cherrychain.it>
2019-09-18 13:14:27 +0200

    Make play interpretable by repl

--- client/src/main.rs
+++ client/src/main.rs
@@ -10,24 +10,21 @@ use std::{env, f32, thread};
 const HOST_ADDRESS: &str = "127.0.0.1:38122";
 const SERVER_ADDRESS: &str = "127.0.0.1:38042";
 
-
 fn main() {
-    let socket: UdpSocket = UdpSocket::bind(HOST_ADDRESS).unwrap();
-
     let steps = 128;
     let step_size: f32 = 2.0 * f32::consts::PI / steps as f32;
     for i in 0.. {
-        let message = play(440. + ((i as f32)*4.));
-        socket.send_to(&message, SERVER_ADDRESS);
+        play(440. + ((i as f32)*4.));
 
         thread::sleep(Duration::from_millis(1000));
     }
 }
 
-fn play(frequency: f32) -> Vec<u8> {
+fn play(frequency: f32) {
     let mut msg_buf = encoder::encode(&OscPacket::Message(OscMessage {
         addr: "/synth/sine".to_string(),
         args: Some(vec![OscType::Float(frequency), OscType::Float(0.)]),
     })).unwrap();
-    msg_buf
+
+    UdpSocket::bind(HOST_ADDRESS).unwrap().send_to(&msg_buf, SERVER_ADDRESS);
 }
