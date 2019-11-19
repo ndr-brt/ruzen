756dd05c6c1f82005e95b65bba3727b02f6cb70b
Andrea Bertagnolli <andrea.bertagnolli@cherrychain.it>
2019-11-19 13:13:09 +0100

    Move plot to another module and make instrument instantiate before

--- src/main.rs
+++ src/main.rs
@@ -31,39 +31,3 @@ fn main() {
     let osc_server = OscServer::new("127.0.0.1:38042");
     osc_server.listen(cmd_out);
 }
-
-// TODO: put plot in a different module
-
-use gnuplot::{Figure, AxesCommon};
-use gnuplot::Coordinate::Graph;
-use gnuplot::PlotOption::Caption;
-use crate::ugen::{ValueAt};
-use crate::ugen::envelope::Envelope;
-
-
-fn plot() {
-    let envelope = Envelope::ar(0.05, 0.5, -4.);
-
-    const SAMPLE_RATE: usize = 1000;
-
-    let mut x_axis: [usize; SAMPLE_RATE as usize] = [0; SAMPLE_RATE as usize];
-    let mut values: [f64; SAMPLE_RATE as usize] = [0.; SAMPLE_RATE as usize];
-    for x in 0..SAMPLE_RATE  {
-        x_axis[x] = x;
-        values[x] = envelope.value_at(x as f64/SAMPLE_RATE as f64);
-    }
-
-    let mut fg = Figure::new();
-    fg.axes2d()
-        .set_title("A plot", &[])
-        .set_legend(Graph(0.5), Graph(0.9), &[], &[])
-        .set_x_label("x", &[])
-        .set_y_label("y^2", &[])
-        .lines(
-            x_axis.iter(),
-            values.iter(),
-            &[Caption("Parabola")],
-        );
-
-    fg.show();
-}
