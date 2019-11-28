20b334b4703f69ef72560ceff6fff9397e6fc861
Andrea Bertagnolli <andrea.bertagnolli@cherrychain.it>
2019-11-28 13:15:03 +0100

    Receive command from upd socket

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use std::thread;
 use crate::out::Out;
 use crate::synth::{Synth, Command };
 use crate::osc_server::OscServer;
+use crate::ui::UIServer;
 
 mod clock;
 mod out;
@@ -17,6 +18,7 @@ mod command_factory;
 mod instrument;
 mod synth;
 mod ugen;
+mod ui;
 
 fn main() {
     let out = Out::init().unwrap_or_else(|e| panic!(e));
@@ -24,10 +26,13 @@ fn main() {
     let (sig_out, sig_in) = sync_channel::<f64>(out.buffer_size());
     let sample_rate = out.sample_rate();
     let synth = Synth::new(sample_rate);
+    let osc_server = OscServer::new("127.0.0.1:38042");
+    let osc_sender = cmd_out.clone();
 
     thread::spawn(move || out.loop_forever(sig_in));
     thread::spawn(move || synth.loop_forever(cmd_in, sig_out));
+    thread::spawn(move || osc_server.listen(osc_sender));
 
-    let osc_server = OscServer::new("127.0.0.1:38042");
-    osc_server.listen(cmd_out);
+    let ui_server = UIServer::new("127.0.0.1:38043");
+    ui_server.listen(cmd_out);
 }
---
+++ src/ui/mod.rs
@@ -0,0 +1,44 @@
+use std::net::{SocketAddrV4, UdpSocket};
+use std::str::FromStr;
+use std::sync::mpsc::Sender;
+use crate::synth::Command;
+
+pub struct UIServer {
+    address: SocketAddrV4,
+}
+
+impl UIServer {
+    pub fn new(address_string: &str) -> Self {
+        UIServer {
+            address: match SocketAddrV4::from_str(address_string) {
+                Ok(address) => address,
+                Err(err) => panic!(err),
+            }
+        }
+    }
+
+    pub fn listen(&self, command_out: Sender<Command>) {
+        let sock = UdpSocket::bind(self.address).unwrap();
+        println!("OSC server listening to {}", self.address);
+
+        let mut buf = [0u8; rosc::decoder::MTU];
+
+        loop {
+            match sock.recv_from(&mut buf) {
+                Ok((size, _address)) => {
+                    println!("Received command from socket");
+                    command_out.send(Command::Instrument(String::from("kick")));
+//                    let packet = rosc::decoder::decode(&buf[..size]).unwrap();
+//                    match message_to_command(packet) {
+//                        Ok(command) => { command_out.send(command); } ,
+//                        Err(err) => { println!("{}", err) }
+//                    }
+                }
+                Err(e) => {
+                    println!("Error receiving from socket: {}", e);
+                    break;
+                }
+            }
+        }
+    }
+}
