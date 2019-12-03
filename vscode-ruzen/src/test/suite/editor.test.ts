c4b978dd55d5d0ee6cf883ba4c636db84b1a7d30
Andrea Bertagnolli <andrea.bertagnolli@cherrychain.it>
2019-12-03 23:14:57 +0100

    Add first test

---
+++ vscode-ruzen/src/test/suite/editor.test.ts
@@ -0,0 +1,75 @@
+import * as assert from 'assert';
+
+import * as TypeMoq from 'typemoq';
+import { Range, TextEditor, TextDocument, Selection, Position, TextLine } from 'vscode';
+import { Editor } from '../../editor';
+
+suite('Editor test suite', () => {
+
+	test('Single line evaluation', () => {
+        let editor = new Editor(createMockEditor(createMockDocument(["cursor is here"]),  new Selection(new Position(0, 3), new Position(0, 3))));
+
+        let expression = editor.currentLineExpression();
+
+		assert.equal(expression.code, "cursor is here");
+		assert.deepEqual(expression.range, new Range(0, 0, 0, 14));
+	});
+});
+
+class TestTextLine implements TextLine {
+    lineNumber: number;
+    text: string;
+    range: Range;
+    rangeIncludingLineBreak: Range;
+    firstNonWhitespaceCharacterIndex: number;
+    isEmptyOrWhitespace: boolean;
+
+    constructor(lineNumber: number, text: string) {
+        this.lineNumber = lineNumber;
+        this.text = text;
+        this.range = new Range(new Position(0, 0), new Position(0, text.length));
+        this.rangeIncludingLineBreak = new Range(new Position(0, 0), new Position(0, text.length + 2));
+        this.firstNonWhitespaceCharacterIndex = text.search('[^\s]');
+        this.isEmptyOrWhitespace = text.trim().length === 0;
+    }
+}
+
+function createMockEditor(document: TextDocument, selection: Selection): TextEditor {
+    let mockEditor = TypeMoq.Mock.ofType<TextEditor>();
+    mockEditor.setup(e => e.document).returns(() => document);
+    mockEditor.setup(e => e.selection).returns(() => selection);
+    return mockEditor.object;
+}
+
+export function createMockDocument(lines: string[]): TextDocument {
+    let mockDocument = TypeMoq.Mock.ofType<TextDocument>();
+    lines.forEach((line, index) => {
+        mockDocument
+            .setup(d => d.lineAt(
+                TypeMoq.It.is((p: Position) => p.line === index && p.character <= line.length)))
+            .returns(() => new TestTextLine(index, line));
+        mockDocument.setup(d => d.lineAt(index))
+            .returns(() => new TestTextLine(index, line));
+    });
+    mockDocument.setup(d => d.lineCount).returns(() => lines.length);
+
+    mockDocument
+        .setup(d => d.getText(TypeMoq.It.isAny()))
+        .returns((r: Range) => {
+            let result = "";
+            for (let line = r.start.line; line <= r.end.line; line++) {
+                if (line === r.start.line) {
+                    result += mockDocument.object.lineAt(line).text.substring(r.start.character);
+                    result += "\r\n";
+                } else if (line < r.end.line) {
+                    result += mockDocument.object.lineAt(line);
+                    result += "\r\n";
+                } else {
+                    result += mockDocument.object.lineAt(line).text.substring(0, r.end.character);
+                }
+            }
+            return result;
+        });
+
+    return mockDocument.object;
+}
