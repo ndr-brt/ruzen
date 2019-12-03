871265d7ad10c925638c231279088a89d57007fc
Andrea Bertagnolli <andrea.bertagnolli@cherrychain.it>
2019-12-03 13:26:09 +0100

    Rename command

--- vscode-ruzen/src/extension.ts
+++ vscode-ruzen/src/extension.ts
@@ -10,7 +10,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	const ruzen = new Ruzen();
 
-	let evalSingleCommand = vscode.commands.registerCommand('ruzen.eval', () => {
+	let evalSingleCommand = vscode.commands.registerCommand('ruzen.lineEval', () => {
 		const activeEditor = vscode.window.activeTextEditor;
 		if (activeEditor) {
 			let currentLine = activeEditor.selection.active.line;
