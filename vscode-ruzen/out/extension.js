7ee15ae7ee38a1668b40984f12053953b4b6ff91
Andrea Bertagnolli <andrea.bertagnolli@cherrychain.it>
2019-12-03 13:04:58 +0100

    Activate extension on eval command

--- vscode-ruzen/out/extension.js
+++ vscode-ruzen/out/extension.js
@@ -6,17 +6,7 @@ var udp = require('dgram');
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "ruzen" is now active!');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
     var client = udp.createSocket('udp4');
     let evalSingleCommand = vscode.commands.registerCommand('ruzen.eval', () => {
         const activeEditor = vscode.window.activeTextEditor;
@@ -43,10 +33,9 @@ function activate(context) {
             }, 250);
         }
     });
-    context.subscriptions.push(disposable, evalSingleCommand);
+    context.subscriptions.push(evalSingleCommand);
 }
 exports.activate = activate;
-// this method is called when your extension is deactivated
 function deactivate() { }
 exports.deactivate = deactivate;
 //# sourceMappingURL=extension.js.map
