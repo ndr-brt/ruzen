cf7e3efbe17835941ea8e337b88e996e74574630
Andrea Bertagnolli <andrea.bertagnolli@cherrychain.it>
2019-12-06 22:39:12 +0100

    Launch first pattern without gluon

--- client/src/main.rs
+++ client/src/main.rs
@@ -3,71 +3,136 @@ extern crate rand;
 extern crate gluon;
 #[macro_use]
 extern crate gluon_vm;
+extern crate crossbeam;
 
 use rosc::encoder;
 use rosc::{OscMessage, OscPacket};
 use std::net::{UdpSocket};
 use std::{thread};
-use gluon::{ThreadExt};
+use gluon::{ThreadExt, RootedThread};
 use gluon::import::add_extern_module;
 use gluon::vm::ExternModule;
 use std::sync::mpsc::{channel};
 use interpreter::GluonInterpreter;
 use std::thread::sleep;
 use std::time::Duration;
+use std::ops::Div;
+use std::sync::Arc;
+use crossbeam::channel::unbounded;
+use crossbeam::channel::Sender;
+use crossbeam::channel::Receiver;
 
 mod interpreter;
 
 const HOST_ADDRESS: &str = "127.0.0.1:38122";
 const SERVER_ADDRESS: &str = "127.0.0.1:38042";
 const INTERPRETER_ADDRESS: &str = "127.0.0.1:38043";
+const CYCLE_TIME: Duration = Duration::from_secs(1);
 
-fn play(command: String) -> String {
-    command
-        .split_whitespace()
-        .for_each(|name| {
-            if name == "~" {
-                sleep(Duration::from_millis(300))
-            } else {
-                let msg_buf = encoder::encode(&OscPacket::Message(OscMessage {
-                    addr: format!("/instrument/{}", name),
-                    args: Some(vec![]),
-                })).unwrap();
+struct Interpreter {
+    sender: Sender<Vec<String>>,
+    vm: RootedThread,
+}
 
-                send_osc_message(msg_buf);
-            }
-    });
+impl Interpreter {
+    fn new(sender: Sender<Vec<String>>) -> Self {
+/*
+        let play = |command: String| {
+            let array = command.split_whitespace().collect::<[&str]>();
+            sender.send(array);
+            command.to_uppercase()
+        };
+*/
 
-    command.to_uppercase()
+        let vm = gluon::new_vm();
+/*        let ah: fn(Interpreter, String) -> String = Interpreter::play;
+        add_extern_module(&vm, "play", |thread| {
+            ExternModule::new(thread, primitive!(1, ah))
+        });
+
+        match vm.load_file("ui.init") {
+            Ok(_) => println!("Init script loaded"),
+            Err(e) => println!("Init script not loaded: {}", e)
+        }
+*/
+        Interpreter { sender, vm }
+    }
+/*
+    fn play(self, command: String) -> String {
+        let array = command.split_whitespace().collect::<Vec<&str>>();
+        self.sender.send(array);
+        command.to_uppercase()
+    }
+    */
+
+    fn execute(&self, command: String) {
+        let array = command
+            .split_whitespace()
+            .map(String::from)
+            .collect::<Vec<String>>();
+
+        self.sender.send(array);
+        /*
+        let result = self.vm
+            .run_expr::<String>("client", code.as_str())
+            .ok();
+
+        match result {
+            Some((val, _arc_type)) => println!("Result: {}", val),
+            None => println!("No result from gluon run_expr")
+        }
+        */
+    }
 }
 
 fn main() {
-    let vm = gluon::new_vm();
-    add_extern_module(&vm, "play", |thread|
-        ExternModule::new(thread, primitive!(1, play))
-    );
+    let (sender, receiver) = unbounded::<Vec<String>>();
 
-    match vm.load_file("ui.init") {
-        Ok(_) => println!("Init script loaded"),
-        Err(e) => println!("Init script not loaded: {}", e)
-    }
+    let interpreter = Interpreter::new(sender);
 
     let (code_out, code_in) = channel::<String>();
 
     thread::spawn(move || loop {
-       match code_in.recv() {
+        match code_in.recv() {
            Ok(code) => {
-               let result = vm
-                   .run_expr::<String>("client", code.as_str())
-                   .ok();
-
-               match result {
-                   Some((val, _arc_type)) => println!("Result: {}", val),
-                   None => println!("No result from gluon run_expr")
-               }
+               interpreter.execute(code.to_string())
            },
            Err(e) => println!("Error receiving code: {}", e)
        }
+    });
+
+    thread::spawn(move || {
+        let mut index = 0;
+        loop {
+            match receiver.recv() {
+                Ok(pattern) => {
+                    println!("New pattern! {}", pattern.len());
+                    thread::spawn(move || {
+                        loop {
+                            if pattern.len() == 0 {
+                                sleep(CYCLE_TIME);
+                                continue;
+                            }
+
+                            if index == pattern.len() {
+                                index = 0;
+                            }
+
+                            let msg_buf = encoder::encode(&OscPacket::Message(OscMessage {
+                                addr: format!("/instrument/{}", pattern[index]),
+                                args: Some(vec![]),
+                            })).unwrap();
+
+                            send_osc_message(msg_buf);
+
+                            sleep(CYCLE_TIME.div(pattern.len() as u32));
+                            index += 1;
+                        }
+                    });
+                },
+                Err(e) => println!("Error {}", e)
+            }
+        }
     });
 
     let interpreter = GluonInterpreter::new(INTERPRETER_ADDRESS);
