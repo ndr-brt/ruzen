cfec30bcc71015182da55d79b24aec2ed2ef2bc1
Andrea Bertagnolli <andrea.bertagnolli@cherrychain.it>
2020-01-15 13:29:18 +0100

    Extract state module

--- server/src/synth/mod.rs
+++ server/src/synth/mod.rs
@@ -5,8 +5,10 @@ use crate::instrument::{snare, kick, Instrument, strange, catta, sine};
 use std::collections::HashMap;
 use rosc::{OscPacket, OscType};
 use crate::synth::parameters::Parameters;
+use crate::synth::state::State;
 
 pub mod parameters;
+mod state;
 
 pub struct Synth {
     sample_rate: Hz,
@@ -46,7 +48,6 @@ impl Synth {
                             any => { println!("OSC command {} not known", any.unwrap()) }
                         }
 
-
                     }
                     OscPacket::Bundle(bundle) => {
                         println!("OSC Bundle: {:?}", bundle);
@@ -63,43 +64,4 @@ impl Synth {
 
     }
 
-}
-
-pub struct State {
-    sample_rate: Hz,
-    instruments: HashMap<String, Box<dyn Instrument>>,
-    definitions: HashMap<String, Box<dyn Fn(f64, Parameters) -> Box<dyn Instrument>>>,
-}
-
-impl State {
-    pub fn new(sample_rate: Hz) -> State {
-        State {
-            sample_rate,
-            instruments: HashMap::new(),
-            definitions: HashMap::new(),
-        }
-    }
-
-    pub fn next_sample(&mut self) -> f64 {
-        self.instruments.retain(|_, instrument| !instrument.is_finished());
-
-        self.instruments.iter_mut().map(|(_, i)| i.signal()).sum()
-    }
-
-    pub fn add(&mut self, name : &str, definition: fn(f64, Parameters) -> Box<dyn Instrument>) {
-        self.definitions.insert(String::from(name), Box::new(definition));
-    }
-
-    pub fn instrument(&mut self, id: String, name: String, params: Parameters) {
-//        println!("Play new instrument: {}. params: {}", name, params); TODO: make it print those params
-        println!("Play new instrument: {}. params: {}", name, params.len());
-        match self.definitions.get(name.as_str()) {
-            Some(function) => { self.instruments.insert(id, function(self.sample_rate, params)); },
-            None => println!("Instrument {} not known", name)
-        }
-    }
-
-    pub fn hush(&mut self) {
-        self.instruments.retain(|_, _| false);
-    }
 }
